73bb1a9862a095b44efa0bc4d68a9c2d94e3149e
guoshijie <guoshijie213>
2023-10-30 19:02:51 +0800

    update theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,8 +18,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'sphinx_rtd_theme',
-    'alabaster',
+    'furo',
 ]
 
 intersphinx_mapping = {
@@ -32,7 +31,7 @@ templates_path = ['_templates']
 html_static_path = ['_static']
 # -- Options for HTML output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 
 html_logo = './_static/favicon.ico'
 
